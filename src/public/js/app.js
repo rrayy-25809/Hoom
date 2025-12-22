db66d23d00df781a4c5476e9d82d4262ba0bba61
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-12-22 21:08:38 +0900

    Changes to be committed: 웹 소켓 메시지 전달 	modified:   src/public/js/app.js 	modified:   src/server.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`); // Connection
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server");
+});
+
+setTimeout(() => {
+    socket.send("Hello Server!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -18,6 +18,21 @@ app.get("/", (req, res) => res.render("home"));
 const server = http.createServer(app); // http 모듈을 사용하여 서버 생성
 const wss = new WebShoket.Server({ server }); // http 서버를 WebSocket에 전달 (두 프로토콜이 같은 포트 공유)
 
+// WebSocket 서버 Event Handling
+wss.on("connection", (socket) => {
+    console.log("Connected to Client");
+
+    socket.on("close", () => {
+        console.log("Disconnected from Client");
+    });
+
+    socket.on("message", (message) => {
+        console.log("New Message: ", message.toString('utf-8'));
+    });
+
+    socket.send("Hello Client!");
+});
+
 server.listen(3000, () => {
     console.log("Server is running at http://localhost:3000");
 });
