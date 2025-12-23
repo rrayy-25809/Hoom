6abcc34a2371a0141778edc6adfc134c217d3dbe
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-12-23 10:55:39 +0900

    Changes to be committed: 브로드캐스팅 학습, 닉네임 기능 추가 	modified:   src/public/js/app.js 	modified:   src/server.js 	modified:   src/views/home.pug

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,40 @@
+const messageList = document.querySelector("ul");
+const nicknameForm = document.getElementById("nickname");
+const messageForm = document.getElementById("message");
 const socket = new WebSocket(`ws://${window.location.host}`); // Connection
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New Message: ", message.data);
+    // console.log("New Message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li)
 });
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server");
 });
 
-setTimeout(() => {
-    socket.send("Hello Server!");
-}, 10000);
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li)
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+});
+
+nicknameForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = nicknameForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+});
--- src/server.js
+++ src/server.js
@@ -18,19 +18,35 @@ app.get("/", (req, res) => res.render("home"));
 const server = http.createServer(app); // http 모듈을 사용하여 서버 생성
 const wss = new WebShoket.Server({ server }); // http 서버를 WebSocket에 전달 (두 프로토콜이 같은 포트 공유)
 
+const sockets = [];
+
 // WebSocket 서버 Event Handling
 wss.on("connection", (socket) => {
+    socket["nickname"] = "Anonymous";
+    sockets.push(socket);
     console.log("Connected to Client");
 
     socket.on("close", () => {
+        const idx = sockets.indexOf(socket);
+        if (idx > -1) sockets.splice(idx, 1); // 연결 종료된 소켓을 배열에서 제거
+
         console.log("Disconnected from Client");
     });
 
     socket.on("message", (message) => {
-        console.log("New Message: ", message.toString('utf-8'));
-    });
+        const parsed = JSON.parse(message.toString('utf-8'));
 
-    socket.send("Hello Client!");
+        switch (parsed.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => {
+                    aSocket.send(`${socket.nickname}: ${parsed.payload}`);
+                });
+                break;
+            case "nickname":
+                socket["nickname"] = parsed.payload;
+                break;
+        }
+    });
 });
 
 server.listen(3000, () => {
